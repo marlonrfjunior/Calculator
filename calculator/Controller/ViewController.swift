318489be314e393483a71790d8a98eeeda6b6fef
Marlon Junior <marlonrfjunior@outlook.com>
2022-09-19 16:45:14 -0300

    Created Model for calculator

--- calculator/Controller/ViewController.swift
+++ calculator/Controller/ViewController.swift
@@ -9,18 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textField: UILabel!
+    
+    var displayValue : Double {
+        get{
+            return Double(textField.text!) ?? 0
+        }
+        set{
+                 if floor(newValue) == newValue {
+                         textField.text = String(Int(newValue))
+                }
+                else{
+                        textField.text = String(newValue)
+                }
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    var calculator = OperatorLogic()
     @IBAction func numberPressed(_ sender: UIButton) {
-        1
+
+        if let textLabel = textField.text , let buttonLabel = sender.titleLabel?.text {
+
+            if buttonLabel != "." {
+                displayValue = Double(textLabel + buttonLabel)!
+                calculator.number = displayValue
+            }
+            else {
+                if !(textLabel.contains(".")){
+                    textField.text = textLabel + "."
+                }
+            }
+        }
     }
     
     
-    @IBAction func operatorPressed(_ sender: Any) {
-        
+    @IBAction func operatorPressed(_ sender: UIButton) {
+        displayValue = calculator.logicCalculate(symbol: sender.titleLabel?.text ?? "")!
     }
     
 }
--- calculator/Model/OperatorLogic.swift
+++ calculator/Model/OperatorLogic.swift
@@ -9,4 +9,55 @@ import Foundation
 
 struct OperatorLogic{
     
+    private var intermeditateCalc : (number1 : Double , operation : String)?
+    
+    var number : Double?
+    
+    mutating func logicCalculate(symbol : String) -> Double?{
+        if let n = self.number {
+            switch symbol{
+            case "%":
+                return n * 0.01
+            case "+/-":
+                return n * -1
+            case "AC":
+                intermeditateCalc = nil
+                number = nil
+                return 0
+            case "=":
+                return calc(num: n, opt: intermeditateCalc!.operation)
+            default:
+                intermeditateCalc!.number1 = calc(num: n, opt: symbol)
+                return 0
+            }
+        }
+        return nil
+    }
+    
+    private mutating func calc(num : Double, opt : String) -> Double{
+        if let n1 = intermeditateCalc?.number1 , let opt = intermeditateCalc?.operation {
+            switch opt {
+            case "÷":
+                intermeditateCalc!.number1 = n1 / num
+                break
+            case "×":
+                intermeditateCalc!.number1 = n1 * num
+                break
+            case "-":
+                intermeditateCalc!.number1 = n1 - num
+                break
+            case "+":
+                intermeditateCalc!.number1 = n1 + num
+                break
+            default:
+                fatalError("Symbol not recognized")
+            }
+            
+        }else{
+            intermeditateCalc = (number1: num , operation: opt)
+        }
+        number = nil
+        return intermeditateCalc!.number1
+    }
+    
 }
